3f4e4dad7c82afdf8f41fa90071ac6cdab3f2583
Your Name <you@example.com>
2025-01-12 21:42:16 -0500

    refactor: Remove mkcert plugin from Vite configuration
    - Updated vite.config.js to remove the mkcert plugin from the plugins array, streamlining the configuration for the Vite build process.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { join, dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
-import mkcert from 'vite-plugin-mkcert'
 
 const path = fileURLToPath(import.meta.url);
 
 export default {
   root: join(dirname(path), "client"),
-  plugins: [ react(), mkcert() ],
+  plugins: [ react() ],
 };
